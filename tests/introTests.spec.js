fcd2ee413cad767cfa21c3d0272d5aec4b6cc294
kaycubria <kaycubria@gmail.com>
2015-06-05 10:52:06 -0500

    adding more testing for http data

--- tests/introTests.spec.js
+++ tests/introTests.spec.js
@@ -2,7 +2,28 @@ describe("My Application", function(){
   beforeEach(module('app'));
 
   describe("my art services", function(){
+    var $httpBackend;
+    var artListService;
 
+    beforeEach(inject(function(_artListService_, _$httpBackend_){
+      $httpBackend = _$httpBackend_;
+      artListService = _artListService_;
+      $httpBackend.when("GET", "data/artworks.json").respond([
+        {
+          "name": "mockPiece",
+          "description": "mockDescription",
+          "artist": "mackPainter"
+        }
+      ]);
+    }));
+
+    it("should fetch the paitings", function(){
+      $httpBackend.expectGET('data/artworks.json');
+      artListService.getArtworks().then(function(data){
+        expect(data[0].name).toBe("mockPiece")
+      });
+      $httpBackend.flush();
+    })
   });
 
   describe("my artCollectionController", function(){
@@ -13,7 +34,8 @@ describe("My Application", function(){
         artList = [{
           name: "Room in Nice",
           artist: "Henri Matisse",
-          description: "Nice"
+          description: "Nice",
+          imageurl: "http://xyz.com"
         }];
 
         $controller('artCollectionController as acc', {$scope: scope, artList: artList});
@@ -22,6 +44,10 @@ describe("My Application", function(){
     it('should set default values', function(){
       expect(scope.acc.personObject.name).toBe('Bob');
       expect(scope.acc.artworks[0].artist).toBe("Henri Matisse")
+    });
+
+    it('should include a link', function(){
+      expect(scope.acc.artworks[0].imageurl).toMatch(/\http.*/);
     });
 
   });
